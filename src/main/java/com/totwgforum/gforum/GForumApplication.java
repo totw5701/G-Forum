7f0b5fe191e154a386149f129116348c3ca30947
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-10-08 20:25:28 +0900

    프로젝트 시작
    Lombok,
    Spring data JPA,
    Mysql,
    Spring Web.

---
+++ src/main/java/com/totwgforum/gforum/GForumApplication.java
@@ -0,0 +1,13 @@
+package com.totwgforum.gforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/totwgforum/gforum/GForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totwgforum.gforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
